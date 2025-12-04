9c6bc135e770d4d628390e49005d9cbd8a96b1b2
poepping <matt.poepping@broadcom.com>
2025-12-04 09:55:50 -0700

    wip

---
+++ 4.py
@@ -0,0 +1,26 @@
+file_name = "data/4_example.txt"
+file_name = "data/4.txt"
+
+puzzle = [ list(x.strip()) for x in open(file_name).readlines() ]
+
+def get_xy(p, x, y):
+    if y < 0 or y >= len(p) or x < 0 or x >= len(p[0]):
+        return False
+    return p[y][x]
+
+def get_around(p, x,y):
+    directions = [ [-1,-1], [0,-1], [1,-1], [-1,0], [1,0], [-1,1], [0,1], [1,1] ]
+    return [ get_xy(p, x+d[0], y+d[1]) for d in directions ]
+
+print(puzzle)
+
+total = 0
+for x in range(0, len(puzzle[0])):
+    for y in range(0, len(puzzle)):
+        if get_xy(puzzle, x,y) == "@":
+            _l = get_around(puzzle, x,y)
+            if _l.count('@') < 4:
+                print(x,y,_l)
+                total +=1
+
+print(total)
