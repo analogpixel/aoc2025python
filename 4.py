de132d0164dd998b31747f00a76d04851b926bc1
poepping <matt.poepping@broadcom.com>
2025-12-04 10:12:28 -0700

    finished day 4

--- 4.py
+++ 4.py
@@ -14,13 +14,45 @@ def get_around(p, x,y):
 
 print(puzzle)
 
-total = 0
-for x in range(0, len(puzzle[0])):
-    for y in range(0, len(puzzle)):
-        if get_xy(puzzle, x,y) == "@":
-            _l = get_around(puzzle, x,y)
-            if _l.count('@') < 4:
-                print(x,y,_l)
-                total +=1
+## part 1 
+# total = 0
+# for x in range(0, len(puzzle[0])):
+#     for y in range(0, len(puzzle)):
+#         if get_xy(puzzle, x,y) == "@":
+#             _l = get_around(puzzle, x,y)
+#             if _l.count('@') < 4:
+#                 print(x,y,_l)
+#                 total +=1
+#
 
-print(total)
+
+## Part 2
+last = -99
+loop_count = 0
+all_total = 0
+while True:
+    total = 0
+    removed = []
+    for x in range(0, len(puzzle[0])):
+        for y in range(0, len(puzzle)):
+            if get_xy(puzzle, x,y) == "@":
+                _l = get_around(puzzle, x,y)
+                if _l.count('@') < 4:
+                    #print(x,y,_l)
+                    removed.append([x,y])
+                    total +=1
+                    all_total +=1 
+
+    for r in removed:
+        x,y = r
+        puzzle[y][x] = '.'
+
+    print("Total for this round:", total)
+
+    if all_total == last:
+        break
+    else:
+        last = all_total
+        loop_count += 1
+
+print(all_total, loop_count)
